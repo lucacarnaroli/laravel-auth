affd0738f64a3164fa94eb2684b21ce3e3e9354a
luca <luca.carnaroli98@gmail.com>
2020-03-30 17:10:03 +0200

    es

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //
+    // SOLO PER ADMIN E GUEST
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
+});
+
+Auth::routes();
+
+Route::prefix('admin')->namespace('Admin')->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
 });
